51c38fed18f70e7cb00b7ef9c143ae85a116b963
Tofazzal26 <afranislamabir6789@gmail.com>
2024-04-11 12:20:52 +0600

    property dynamic validation implement finish

--- src/Pages/Property/Property.jsx
+++ src/Pages/Property/Property.jsx
@@ -5,7 +5,8 @@ const Property = () => {
   const { stateData, loading } = useHook();
   return (
     <div>
-      <div>
+      <h1 className="text-center text-4xl my-6 font-semibold">Our Property</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {stateData.map((cardPr) => (
           <PropertyCard key={cardPr.id} cardPr={cardPr} />
         ))}
--- src/Pages/Property/PropertyCard.jsx
+++ src/Pages/Property/PropertyCard.jsx
@@ -1,9 +1,31 @@
+import { NavLink } from "react-router-dom";
+
 const PropertyCard = ({ cardPr }) => {
-  const { image, location, facilities } = cardPr;
+  const { image, location, facilities, estate_title, id } = cardPr;
 
   return (
     <div>
-      <h1>Property Card</h1>
+      <div className="card card-compact lg:h-[500px] rounded-none shadow-loginSd">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl">{estate_title}</h2>
+          <div className="flex items-center text-[#f7505c] text-base ">
+            <p>{facilities[0]}</p>
+            <p>{facilities[1]}</p>
+            <p>{facilities[2]}</p>
+          </div>
+          <p className="font-semibold text-lg text-gray-600">{location}</p>
+          <div className="card-actions">
+            <NavLink to={`/viewProperty/${id}`}>
+              <button className="bg-[#1d1f1f] text-white flex items-center text-[18px] font-semibold gap-4 px-4 py-4">
+                Show Property
+              </button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
